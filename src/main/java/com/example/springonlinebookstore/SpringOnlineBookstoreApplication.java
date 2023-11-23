4fcc27037f2b135b2ecc81f021cf9c1b8600a84f
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-11-23 12:29:54 +0200

    creted project, added some properties

---
+++ src/main/java/com/example/springonlinebookstore/SpringOnlineBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.springonlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOnlineBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOnlineBookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springonlinebookstore/SpringOnlineBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springonlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOnlineBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
