0dd385d501cfbb291d054712a407c74a7c4f3297
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-11-29 20:45:43 +0200

    deletet anotation component in BookDto class

--- src/main/java/com/example/springonlinebookstore/dto/BookDto.java
+++ src/main/java/com/example/springonlinebookstore/dto/BookDto.java
@@ -2,10 +2,8 @@ package com.example.springonlinebookstore.dto;
 
 import java.math.BigDecimal;
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
 @Data
-@Component
 public class BookDto {
 
     private Long id;
