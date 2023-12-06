05232160372a79f28ddc9644859548563a0168cd
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-12-06 18:00:02 +0200

    added liquibase in pom.xml and added changelog for book

--- src/main/java/com/example/springonlinebookstore/model/Book.java
+++ src/main/java/com/example/springonlinebookstore/model/Book.java
@@ -6,7 +6,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
